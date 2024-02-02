ebfc20fc4a53c543b7517efc78c9674ece40d3d3
eastanganelli <ezequielstanganelli@gmail.com>
2024-02-02 01:05:26 -0300

    first commit

---
+++ schema_data/query_testing_data.sql
@@ -0,0 +1,29 @@
+﻿USE STEL_DB_DATA;
+
+/*
+set @idSample = NULL;
+#CALL insertSample('ISO-1167-4560', 'PE', 'PE100', 50, 50, 5, 340, 560, 25, 20, @idSample);
+CALL insertSample('ISO-1167-4560', 'PE', 'PE50', 50, 50, 5, 180, 430, 34, 45, @idSample);
+SELECT @idSample;
+
+
+SELECT
+COUNT(ts.test_sample)
+FROM test_specimen ts
+WHERE ts.test_sample = idSample;
+*/
+
+# SELECT specimenExists(1, '2024/01/5 03:17:00') AS RESULTspecimenExists;
+
+/*
+SELECT
+ts.id
+FROM test_specimen ts
+WHERE (ts.test_sample = 1 AND ts.start = '2024/01/5 03:17:00')
+LIMIT 1;
+*/
+
+CALL insertSample('ISO-1167-1996', 'PE', 'PE100', 50, 50, 5, 350, 560, 25, 20, '3 h ± 15 min');
+CALL insertSpecimen(1,1,'water');
+
+CALL insertData(13,0.07,0);
---
+++ schema_front/query_data.sql
@@ -0,0 +1,52 @@
+﻿USE STEL_DB_STATIC;
+
+#
+# CALL getStandardTree();
+
+## Testing Specifications by Material
+#CALL getSpecificationsByMaterial();
+
+# CALL getConcPeriodsbyStandard(0);
+
+# CALL getStandardSpecsDataList(0);
+
+# CALL getDataBySpecAndStandard(1,0);
+
+/*
+CREATE TEMPORARY TABLE IF NOT EXISTS testing(
+SELECT
+m.id AS idMaterial
+FROM period_by_specification pbs
+INNER JOIN specification s ON pbs.specification = s.id
+INNER JOIN material m ON s.material = m.id
+WHERE pbs.standard = 0
+GROUP BY idMaterial, pbs.standard
+);
+
+SELECT
+pbs.standard AS idStandard,
+pbs.specification AS idSpecification
+FROM period_by_specification pbs
+INNER JOIN specification s ON pbs.specification = s.id
+WHERE pbs.standard = 0 AND s.material IN (SELECT * FROM testing)
+GROUP BY idSpecification;
+*/
+
+/*
+SELECT
+pbs.standard AS idStandard,
+m.id AS idMaterial,
+m.name AS materialName,
+m.type AS materialType,
+s.id AS idSpecification,
+s.name AS specificationName,
+pbs.temperature AS temperature,
+pbs.hours AS hours,
+pbs.seconds AS seconds
+FROM period_by_specification pbs
+INNER JOIN specification s ON pbs.specification = s.id
+INNER JOIN material m ON s.material = m.id;
+*/
+
+# CALL get_SpecificationByStandard(0,0);
+CALL get_ConditionalPeriodsbyStandard(0);
