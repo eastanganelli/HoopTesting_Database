8ce609a06fb456710db320193621460cdf03bc55
Ezequiel A. Stanganelli <ezequielstanganelli@gmail.com>
2024-05-29 22:42:21 -0300

    changes in DB procedures

---
+++ query_select_manager.sql
@@ -0,0 +1,34 @@
+﻿SELECT
+  s.id,
+  s.name,
+  (
+    SELECT
+      JSON_ARRAYAGG(JSON_OBJECT(
+        'id', e.id,
+        'type', e.encap
+      ))
+    FROM endcap e
+    WHERE e.standard = s.id
+  ) AS `endCaps`,
+  (
+    SELECT
+      JSON_ARRAYAGG(JSON_OBJECT(
+        'id', e.id,
+        'insertFluid', e.insertFluid,
+        'outsideLfuid', e.outFluid
+      ))
+    FROM enviroment e
+    WHERE e.standard = s.id
+  ) AS `enviroment`,
+  (
+    SELECT
+      JSON_ARRAYAGG(JSON_OBJECT(
+        'id', cp.id,
+        'minwall', cp.minwall,
+        'maxwall', cp.maxwall,
+        'time', cp.time 
+      ))
+    FROM conditional_period cp
+    WHERE cp.standard = s.id
+  ) AS conditionalPeriod
+FROM standard s
